f4843830ee827f14fd948329747c07845c4fac90
MatheusAssisPJeronimo <matheus.jeronimo@alunos.ifsuldeminas.edu.br>
2024-04-09 13:48:56 -0300

    Aula 7

---
+++ Aula7/revisao.py
@@ -0,0 +1,15 @@
+#instalar o pyautogui
+#pip insltall pyautogui
+
+import pyautogui
+
+"""
+mover a seta do mouse ate 
+respectivamente as coordenadas (X, Y, e a velocidade)
+"""
+pyautogui.moveTo(1112, 1052, duration=.5)
+
+"""
+
+"""
+
