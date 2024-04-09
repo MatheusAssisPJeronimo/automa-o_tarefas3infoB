48b7e15eba71e0e1eaa4e5cca2e653ef0b1a396a
MatheusAssisPJeronimo <matheus.jeronimo@alunos.ifsuldeminas.edu.br>
2024-04-09 14:32:24 -0300

    Automacao

---
+++ Aula7/SaveGit.py
@@ -0,0 +1,4 @@
+import pyautogui
+
+btnXY = pyautogui.locateCenterOnScreen('./aula7/git_btn.png')
+pyautogui.click(btnXY, duration=.2)
--- Aula7/revisao.py
+++ Aula7/revisao.py
@@ -6,10 +6,23 @@ import pyautogui
 """
 mover a seta do mouse ate 
 respectivamente as coordenadas (X, Y, e a velocidade)
-"""
+
+#move de acordo com os paramentros da tela
 pyautogui.moveTo(1112, 1052, duration=.5)
+#move de acordo com o ponto do mouse levando ele como o ponto (0, 0)
+pyautogui.moveRel(0, 100, duration=.5)
+pyautogui.moveRel(100, 0, duration=.5)
+"""
+# salvar o Arquivo
+#pyautogui.moveTo(746, 91, duration=.5)
+#pyautogui.click(746, 91, duration=.2)
+#pyautogui.moveTo(757, 415, duration=.5)
+#pyautogui.click(757, 415, duration=.2)
 
 """
-
+Localizar um item na tela
 """
-
+btnXY = pyautogui.locateCenterOnScreen('./aula7/btn_edit.png')
+pyautogui.click(btnXY, duration=.2)
+btnXY = pyautogui.locateCenterOnScreen('./aula7/btnSave.png')
+pyautogui.click(btnXY, duration=.2)
